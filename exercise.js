8b48a2002f96132f91fdb9fc8e60f6b7f68e3a57
moses <tinuoyemosesade@gmail.com>
2021-09-15 12:32:27 +0200

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -13,7 +13,6 @@ game.name = [];
 game.name[0] ="mrs white";
 game.name.push('mr green')
 
-
 // destructuring
 {"name":"Rusty", "room": "kitchen", "weapon":"candlestick"}
 const { name, weapon, room } = {
